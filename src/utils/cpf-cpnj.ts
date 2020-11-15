827ebf0ccc0b02c1cf9ee74e1eff5adab05c4853
Jhonatas Santos <jhonatassantos@MacBook-Pro-de-Jhonatas.local>
2020-11-15 17:44:31 -0300

    feat: added formatted CPF util

---
+++ src/utils/cpf-cpnj.ts
@@ -0,0 +1,20 @@
+export const cpfCnpjMask = (value: any): string => {
+  value = value.replace(/\D/g, '')
+
+  if (value.length < 14) {
+    value = value.replace(/(\d{3})(\d)/, '$1.$2')
+    value = value.replace(/(\d{3})(\d)/, '$1.$2')
+    value = value.replace(/(\d{3})(\d{1,2})$/, '$1-$2')
+  } else {
+    value = value.replace(/^(\d{2})(\d)/, '$1.$2')
+    value = value.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3')
+    value = value.replace(/\.(\d{3})(\d)/, '.$1/$2')
+    value = value.replace(/(\d{4})(\d)/, '$1-$2')
+  }
+
+  return value
+}
+
+export const cpfCnpjUnmask = (value: string): string => {
+  return value.replace(/\D/g, '')
+}
